c54ed1a83236f74cd61baf55fa323e56b3d81a3a
tuv292 <tuv292@gmail.com>
2020-08-24 21:08:53 -0400

    Fixing tests

--- tests/testthat/test-abx_index.R
+++ tests/testthat/test-abx_index.R
@@ -2,11 +2,11 @@ testthat::context("Testing abx_indx functionality")
 
 testthat::test_that("Testing abx_idx on test df", {
 
-  idx_outcome <- abx_index(test_df, "Vancomycin")
+  idx_outcome <- vancomycin_index(test_df)
 
   testthat::expect_type(idx_outcome, "list")
   testthat::expect_equal(length(idx_outcome), 4)
-  testthat::expect_equal(idx_outcome > 0, c(a = TRUE, b = FALSE, c = TRUE, d = FALSE)) ##test that there are two samples have positive idx indices
+  testthat::expect_equal(idx_outcome > 0, c(a = FALSE, b = FALSE, c = TRUE, d = FALSE)) ##test that there are two samples have positive idx indices
 
   ##write tests for the df as well
 
