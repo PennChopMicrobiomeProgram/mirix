a2592011fcb1faf42a5a19c3d6f95641b3ad26c5
Kyle Bittinger <kylebittinger@gmail.com>
2021-06-14 10:55:27 -0400

    Merge documentation for antibiotic-specific indices

--- R/abx_index.R
+++ R/abx_index.R
@@ -1,20 +1,29 @@
-#' Calculate the antibiotic index for vancomycin
+#' Calculate antibiotic-specific index values
 #'
 #' @param abundance A vector of taxon abundances in a sample
 #' @param lineage A character vector of taxonomic assignments or lineages
+#' @param replace_zero Zero-replacement value. If the numerator or denominator
+#'   is smaller than this value, they will be replaced with the number here.
+#'   This number should generally be slightly less than the minimum measurement
+#'   that could be acquired with the method used. For count data, a value of
+#'   0.5 is typical. For relative abundances, a number that is slightly lower
+#'   than the lowest relative abundance will work.
 #' @param antibiotic_db A data frame with columns named "taxon", "rank",
 #'   "antibiotic", and "value"
-#' @param phenotype_db A data frame with columns named "taxon", "rank", and
-#'   "gram_stain"
-#'
-#' @return The vancomycin antibiotic index for the sample
-#' @export
-#'
+#' @param phenotype_db A data frame with columns named "taxon", "rank",
+#'   "gram_stain", and "aerobic_status"
+#' @name antibiotic_sepecific_index
 #' @examples
 #' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
 #' vancomycin_index(h22$proportion, h22$lineage)
+NULL
+
+
+#' @rdname antibiotic_sepecific_index
+#' @export
 vancomycin_index <- function(abundance,
                              lineage,
+                             replace_zero = 1e-4,
                              antibiotic_db = taxon_susceptibility,
                              phenotype_db = taxon_phenotypes) {
   susceptibility <- vancomycin_susceptibility(
@@ -22,66 +31,33 @@ vancomycin_index <- function(abundance,
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for tetracycline
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param antibiotic_db A data frame with columns named "taxon", "rank",
-#'   "antibiotic", and "value"
-#'
-#' @return The tetracycline antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' tetracycline_index(h22$proportion, h22$lineage)
 tetracycline_index <- function(abundance,
                                lineage,
+                               replace_zero = 1e-4,
                                antibiotic_db = taxon_susceptibility) {
   susceptibility <- tetracycline_susceptibility(
     lineage, antibiotic_db)
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for penicillin-like antibiotics
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param antibiotic_db A data frame with columns named "taxon", "rank",
-#'   "antibiotic", and "value"
-#'
-#' @return The penicillin antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' penicillin_index(h22$proportion, h22$lineage)
 penicillin_index <- function(abundance,
                              lineage,
+                             replace_zero = 1e-4,
                              antibiotic_db = taxon_susceptibility) {
   susceptibility <- penicillin_susceptibility(
     lineage, antibiotic_db)
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for Gram-positive bacteria
-#'
-#' Antibiotics such as glycopeptides, macrolides, oxazolidinones, lincosamides,
-#' and lipopeptides aside from vancomycin.
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param phenotype_db A data frame with columns named "taxon", "rank", and
-#'   "gram_stain"
-#'
-#' @return The Gram-positive antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' gram_positive_index(h22$proportion, h22$lineage)
 gram_positive_index <- function(abundance,
                            lineage,
+                           replace_zero = 1e-4,
                            phenotype_db = taxon_phenotypes) {
   susceptibility <- phenotype_susceptibility(
     lineage = lineage,
@@ -93,23 +69,11 @@ gram_positive_index <- function(abundance,
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for Gram-negative bacteria
-#'
-#' Antibiotics such as polymyxin and aztreonam.
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param phenotype_db A data frame with columns named "taxon", "rank", and
-#'   "gram_stain"
-#'
-#' @return The Gram-negative antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' gram_negative_index(h22$proportion, h22$lineage)
 gram_negative_index <- function(abundance,
                            lineage,
+                           replace_zero = 1e-4,
                            phenotype_db = taxon_phenotypes) {
   susceptibility <- phenotype_susceptibility(
     lineage = lineage,
@@ -121,23 +85,11 @@ gram_negative_index <- function(abundance,
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for obligate anaerboes
-#'
-#' Antibiotics such as nitroimidazole.
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param phenotype_db A data frame with columns named "taxon", "rank", and
-#'   "aerobic_status"
-#'
-#' @return The obligate anaerobe antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' anaerobes_index(h22$proportion, h22$lineage)
 anaerobes_index <- function(abundance,
                             lineage,
+                            replace_zero = 1e-4,
                             phenotype_db = taxon_phenotypes) {
   susceptibility <- phenotype_susceptibility(
     lineage = lineage,
@@ -150,23 +102,11 @@ anaerobes_index <- function(abundance,
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for aerobes and facultative anaerobes
-#'
-#' Antibiotics such as fluoroquinolones.
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param phenotype_db A data frame with columns named "taxon", "rank", and
-#'   "aerobic_status"
-#'
-#' @return The aerobe antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' aerobes_index(h22$proportion, h22$lineage)
 aerobes_index <- function(abundance,
                           lineage,
+                          replace_zero = 1e-4,
                           phenotype_db = taxon_phenotypes) {
   susceptibility <- phenotype_susceptibility(
     lineage = lineage,
@@ -179,23 +119,11 @@ aerobes_index <- function(abundance,
   antibiotic_index(abundance, susceptibility)
 }
 
-#' Calculate the antibiotic index for aminoglycoside antibiotics
-#'
-#' @param abundance A vector of taxon abundances in a sample
-#' @param lineage A character vector of taxonomic assignments or lineages
-#' @param antibiotic_db A data frame with columns named "taxon", "rank",
-#'   "antibiotic", and "value"
-#' @param phenotype_db A data frame with columns named "taxon", "rank", and
-#'   "gram_stain"
-#'
-#' @return The aminoglycoside antibiotic index for the sample
+#' @rdname antibiotic_sepecific_index
 #' @export
-#'
-#' @examples
-#' h22 <- weiss2021_data[weiss2021_data$sample_id %in% "Healthy.22",]
-#' aminoglycoside_index(h22$proportion, h22$lineage)
 aminoglycoside_index <- function(abundance,
                                  lineage,
+                                 replace_zero = 1e-4,
                                  antibiotic_db = taxon_susceptibility,
                                  phenotype_db = taxon_phenotypes) {
   susceptibility <- aminoglycoside_susceptibility(
