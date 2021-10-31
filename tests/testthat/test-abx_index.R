80540bb9dbd19d8edee470d63ef41e25c7714819
Kyle Bittinger <kylebittinger@gmail.com>
2021-10-31 18:58:14 -0400

    Fix broken tests

--- tests/testthat/test-abx_index.R
+++ tests/testthat/test-abx_index.R
@@ -87,13 +87,13 @@ test_that("penicillin_index works on Weiss examples", {
 test_that("aminoglycoside_susceptibility works on Weiss examples", {
   expect_equal(
     aminoglycoside_susceptibility(weiss_sepsis$lineage),
-    c("resistant", "susceptible", "resistant", "resistant", "susceptible"))
+    c("susceptible", "resistant", "resistant", "resistant", "resistant"))
 
   expect_equal(
     aminoglycoside_susceptibility(weiss_healthy$lineage),
     c("resistant", "resistant", "resistant", "resistant", "resistant",
       "resistant", "resistant", "resistant", "resistant", "resistant",
-      "resistant", "resistant", "susceptible", "resistant", "resistant",
+      "resistant", "resistant", "resistant", "resistant", "resistant",
       "resistant", "resistant", "resistant", "resistant", "resistant",
       NA, NA, "resistant", "resistant", "resistant", "resistant"))
 })
@@ -101,11 +101,10 @@ test_that("aminoglycoside_susceptibility works on Weiss examples", {
 test_that("aminoglycoside_index works on Weiss examples", {
   expect_equal(
     aminoglycoside_index(weiss_sepsis$proportion, weiss_sepsis$lineage),
-    0.5644559, tolerance = 1e-5)
-
+    -0.52420, tolerance = 1e-5)
   expect_equal(
     aminoglycoside_index(weiss_healthy$proportion, weiss_healthy$lineage),
-    2.21396, tolerance = 1e-5)
+    3.99476, tolerance = 1e-5)
 })
 
 test_that("gram_positive_index works on Weiss examples", {
