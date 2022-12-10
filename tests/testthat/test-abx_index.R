b46190dcd93a4d070f4dc2ee96dfd88fccd2a8f4
Kyle Bittinger <kylebittinger@gmail.com>
2022-12-10 18:28:57 -0500

    Fix tests for sign change

--- tests/testthat/test-abx_index.R
+++ tests/testthat/test-abx_index.R
@@ -23,11 +23,11 @@ test_that("antibiotic_susceptibility_vancomycin works on Weiss examples", {
 test_that("mirix_vancomycin works on Weiss examples", {
   expect_equal(
     mirix_vancomycin(weiss_sepsis$proportion, weiss_sepsis$lineage),
-    0.5670429, tolerance = 1e-5)
+    -0.5670429, tolerance = 1e-5)
 
   expect_equal(
     mirix_vancomycin(weiss_healthy$proportion, weiss_healthy$lineage),
-    0.3609116, tolerance = 1e-5)
+    -0.3609116, tolerance = 1e-5)
 })
 
 test_that("antibiotic_susceptibility_tetracycline works on Weiss examples", {
@@ -49,11 +49,11 @@ test_that("antibiotic_susceptibility_tetracycline works on Weiss examples", {
 test_that("mirix_doxycycline works on Weiss examples", {
   expect_equal(
     mirix_doxycycline(weiss_sepsis$proportion, weiss_sepsis$lineage),
-    -1.879934, tolerance = 1e-5)
+    1.879934, tolerance = 1e-5)
 
   expect_equal(
     mirix_doxycycline(weiss_healthy$proportion, weiss_healthy$lineage),
-    -1.045228, tolerance = 1e-5)
+    1.045228, tolerance = 1e-5)
 })
 
 test_that("antibiotic_susceptibility_penicillin works on Weiss examples", {
@@ -74,11 +74,11 @@ test_that("antibiotic_susceptibility_penicillin works on Weiss examples", {
 test_that("mirix_amoxicillin works on Weiss examples", {
   expect_equal(
     mirix_amoxicillin(weiss_sepsis$proportion, weiss_sepsis$lineage),
-    -1.879934, tolerance = 1e-5)
+    1.879934, tolerance = 1e-5)
 
   expect_equal(
     mirix_amoxicillin(weiss_healthy$proportion, weiss_healthy$lineage),
-    -1.045228, tolerance = 1e-5)
+    1.045228, tolerance = 1e-5)
 })
 
 test_that("antibiotic_susceptibility_aminoglycoside works on Weiss examples", {
@@ -92,33 +92,31 @@ test_that("antibiotic_susceptibility_aminoglycoside works on Weiss examples", {
       "resistant", "resistant", "resistant", "resistant", "resistant",
       "resistant", "resistant", "resistant", "resistant", "resistant",
       "resistant", "resistant", "resistant", "resistant", "resistant",
-      NA, NA, "resistant", "resistant", "resistant", "resistant"))
+      NA, "susceptible", "resistant", "resistant", "resistant", "resistant"))
 })
 
 test_that("mirix_gentamicin works on Weiss examples", {
   expect_equal(
     mirix_gentamicin(weiss_sepsis$proportion, weiss_sepsis$lineage),
-    -0.52420, tolerance = 1e-5)
+    0.52420, tolerance = 1e-5)
   expect_equal(
     mirix_gentamicin(weiss_healthy$proportion, weiss_healthy$lineage),
-    3.99476, tolerance = 1e-5)
+    -2.69373, tolerance = 1e-5)
 })
 
 test_that("mirix_metronidazole works on Weiss examples", {
   expect_equal(
     mirix_metronidazole(weiss_sepsis$proportion, weiss_sepsis$lineage),
-    1.788434, tolerance = 1e-5)
-
+    -1.788434, tolerance = 1e-5)
   expect_equal(
     mirix_metronidazole(weiss_healthy$proportion, weiss_healthy$lineage),
-    -2.21396, tolerance = 1e-5)
+    2.089021, tolerance = 1e-5)
 })
 
 test_that("mirix_metronidazole and mirix_ciprofloxacin are inverses", {
   expect_equal(
     mirix_metronidazole(weiss_sepsis$proportion, weiss_sepsis$lineage),
     -mirix_ciprofloxacin(weiss_sepsis$proportion, weiss_sepsis$lineage))
-
   expect_equal(
     mirix_metronidazole(weiss_healthy$proportion, weiss_healthy$lineage),
     -mirix_ciprofloxacin(weiss_healthy$proportion, weiss_healthy$lineage))
