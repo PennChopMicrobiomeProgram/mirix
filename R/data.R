13972ee1c367742993262692d2f4069fc2851ad6
Ulthran <ctbushman@gmail.com>
2024-06-20 15:55:36 -0400

    Remove data.R refs and rda files

--- R/data.R
+++ R/data.R
@@ -1,31 +1,3 @@
-#' Gram stain and aerobic status of bacterial taxa
-#' @format A data frame with the following columns:
-#' \describe{
-#'   \item{taxon}{The name of the taxon}
-#'   \item{rank}{The rank of the taxon}
-#'   \item{aerobic_status}{
-#'     The aerobic status. One of "aerobe", "facultative anaerobe", or
-#'     "obligate anaerobe".}
-#'   \item{gram_stain}{
-#'     How the taxon appears when Gram-stained. One of "Gram-positive" or
-#'     "Gram-negative".}
-#'   \item{doi}{DOI of the publication from which the information was obtained.}
-#' }
-"taxon_phenotypes"
-
-#' Antibiotic susceptibility of bacterial taxa
-#' @format A data frame with the following columns:
-#' \describe{
-#'   \item{taxon}{The name of the taxon}
-#'   \item{rank}{The rank of the taxon}
-#'   \item{antibiotic}{The antibiotic or antibiotic class}
-#'   \item{value}{
-#'     The susceptibility of the taxon to the antibiotic, one of "susceptible"
-#'     or "resistant".}
-#'   \item{doi}{DOI of the publication from which the information was obtained.}
-#' }
-"taxon_susceptibility"
-
 #' Example data from Weiss et al.
 #'
 #' @format A data frame with the following columns:
